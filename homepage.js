06d5d60420760c223e3cd77514c3a5f1122e94d5
Katherine Sullivan <ksulli@pointpark.edu>
2018-09-20 20:51:00 -0400

    Working pages

--- homepage.js
+++ homepage.js
@@ -25,20 +25,17 @@ app.get("/about", function(req, res){
 
 app.use(express.static(__dirname + "/public"));
 
-// custom 404 page
-app.use(function(req, res){
-	res.type("text/plain");
+app.use(function(req, res, next){
 	res.status(404);
-	res.send("404 - Not Found");
+	res.render("404");
 });
 
-//custom 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type("text/plain");
 	res.status(500);
-	res.send("500 - server Error");
+	res.render("500");
 });
+
 app.listen(app.get("port"), function(){
 	console.log( "Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
 });
